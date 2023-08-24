ed9895202c88b94bbdd305560d57c524bad2f5ce
Renatatr <78363362+Renatatr@users.noreply.github.com>
2023-08-24 10:34:48 -0300

    Chamando nome de tabelas e de colunas implicitamente

--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Alura.Filmes.App
+﻿using System;
+using Alura.Filmes.App.Dados;
+
+namespace Alura.Filmes.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            //select * from actor
+            using (var contexto = new AluraFilmesContexto())
+            {
+                foreach (var item in contexto.Atores) {
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
